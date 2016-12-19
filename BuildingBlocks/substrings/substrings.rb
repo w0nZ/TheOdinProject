63bfb6f403baa64087a708813100ebf695e53e16
Fabian Bialowons <fabian.bialowons@gmail.com>
2016-12-19 13:41:45 +0100

    finished substrings with scan().length

--- BuildingBlocks/substrings/substrings.rb
+++ BuildingBlocks/substrings/substrings.rb
@@ -1,2 +1,16 @@
 #substrings
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings ( string , dict )
+	counts = Hash.new(0)
+	string.downcase!
+	dict.each do |word|
+		if string.include?(word)
+			counts[word] = string.scan(word).length
+		end
+	end
+	puts counts
+end
+
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
