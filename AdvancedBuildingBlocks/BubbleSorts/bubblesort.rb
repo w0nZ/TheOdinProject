ac1e1acddc7d4b33a445da8618e85bf705b4f751
Fabian Bialowons <fabian.bialowons@gmail.com>
2016-12-20 19:10:03 +0100

    added def bubble_sort

--- AdvancedBuildingBlocks/BubbleSorts/bubblesort.rb
+++ AdvancedBuildingBlocks/BubbleSorts/bubblesort.rb
@@ -1,1 +1,19 @@
 #bubblesort
+
+def bubble_sort(arr)
+	n = arr.length
+	loop do
+	swapped = false
+		for i in (1..n-1)
+			if arr[i-1] > arr[i]
+				arr[i-1], arr[i] = arr[i], arr[i-1]
+				swapped = true
+			end
+		end
+		break if swapped == false
+	end
+	return arr
+end
+
+array = [4,3,78,2,0,2]
+puts bubble_sort(array)
