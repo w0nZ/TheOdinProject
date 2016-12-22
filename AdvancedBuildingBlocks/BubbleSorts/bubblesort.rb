dbd3150aa61f7601910776476ae6d4ad43a83d36
Fabian Bialowons <fabian.bialowons@gmail.com>
2016-12-22 09:19:06 +0100

    bubble_sort_by added with yield

--- AdvancedBuildingBlocks/BubbleSorts/bubblesort.rb
+++ AdvancedBuildingBlocks/BubbleSorts/bubblesort.rb
@@ -17,3 +17,21 @@ end
 
 array = [4,3,78,2,0,2]
 puts bubble_sort(array)
+
+def bubble_sort_by(arr)
+	n = arr.length
+	loop do
+	swapped = false
+		for i in (1..n-1)
+			y = yield arr[i-1],arr[i]
+			if yield(arr[i-1],arr[i]) >= 1
+				arr[i-1], arr[i] = arr[i], arr[i-1]
+				swapped = true
+			end
+		end
+		break if swapped == false
+	end
+	return arr
+end
+
+puts bubble_sort_by(["hey","hi","hello","hola","hi","hello","hey"]) { |left,right| left.length - right.length }
