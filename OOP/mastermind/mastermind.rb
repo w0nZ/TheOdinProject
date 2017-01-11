3f0904fc48b966d880dcd5ec16c5888f743c9794
Fabian Bialowons <fabian.bialowons@gmail.com>
2017-01-11 06:25:59 +0100

    Added some comments

--- OOP/mastermind/mastermind.rb
+++ OOP/mastermind/mastermind.rb
@@ -29,11 +29,10 @@ class Mastermind
 	end
 
 	def cheat
-		##CheatMode
+		##print the choosen Code on Screen
 		print "Secret Code: "
 		@code.each { |x| print "#{x} "}
 		print "\n"
-		##CheatMode
 	end
 	
 	def welcome
@@ -44,10 +43,10 @@ class Mastermind
 	end
 	
 	def turn
+		# Who needs to guess, ki or human, save guess, increment game round
 		if @mode=="guess"
 			@guess[@round-1] = @human.guess(@round)
 		else
-			puts "not yet implemented"
 			@guess[@round-1] = @ki.guess(@round,@feedback[@round-2],@guess[@round-2])
 		end
 		compare
@@ -57,23 +56,27 @@ class Mastermind
 	
 	def finished? 
 	#Check if finishd
-		if @feedback[@round-2] == ["!", "!", "!", "!"] && @mode == "guess"
+		fin = true
+		if won?
 			puts "##### Congrats you won #####"
-			return true
-		elsif @feedback[@round-2] == ["!", "!", "!", "!"] && @mode == "code"
+		elsif lost? 
 			puts "##### you lost #####"
-			return true
-		elsif @mode == "guess" && @round > 12
-			puts "##### you lost #####"
-			return true		
-		elsif @mode == "code" && @round > 12
-			puts "##### Congrats you won #####"
-			return true	
+		else
+			fin = false
 		end
+		fin
+	end
+	
+	def won?
+		true if @feedback[@round-2] == ["!", "!", "!", "!"] && @mode == "guess" || @mode == "code" && @round > 13
+	end
+	
+	def lost?
+		true if @feedback[@round-2] == ["!", "!", "!", "!"] && @mode == "code" || @mode == "guess" && @round > 13
 	end
 	
 	def compare
-	#compare the actual guesses with the code save feedback in @feedback
+	#compare the actual guesses with the code saved in feedback
 		i = 0
 		comp = []
 		@guess[@round-1].each do |val|
@@ -94,6 +97,7 @@ class Mastermind
 	end
 	
 	class Player
+	#Every Player has a name (even the KI)
 		attr_accessor :name
 
 		def initialize(name)
@@ -102,9 +106,10 @@ class Mastermind
 	end
 	
 	class Human < Player
-
+	# Human class
 		def guess(round)
-			puts "Hi #{self.name} this is round #{round}"
+			# get a guess, validate and split to arr
+			puts "\n##### #{self.name} this is round #{round}"
 			input = enterCode
 			#puts "Input: #{input}"
 			inputVal?(input)
@@ -112,6 +117,7 @@ class Mastermind
 		end
 		
 		def inputVal?(input)
+		#input Validation for guess or code gen
 			while !(input =~ /[A-F]/ && input.length == 4) do
 				puts "Sorry, valid input is A..F, 4 chars"
 				input = gets.chomp
@@ -119,12 +125,14 @@ class Mastermind
 		end
 		
 		def enterCode
+			#gets a guess code or a secrete code
 			puts "Please enter your Code, 4 chars A..F (f.e. 'ABCD'):"
 			input = gets.chomp
 			return input
 		end
 		
 		def genCode
+			# get a secret code.. very equal to "def guess".. stinks like redundant code
 			input = enterCode
 			inputVal?(input)
 			input.split("")
@@ -132,7 +140,9 @@ class Mastermind
 	end
 	
 	class Ki < Player 
+	#Ki class from Player
 		def genCode
+			#gen 4 random chars and safe in arr
 			code = []
 			for i in 0..3
 				code[i] = genChar
@@ -141,12 +151,14 @@ class Mastermind
 		end
 		
 		def genChar
+			#generate a Random char from A to F
 			(65 + rand(6)).chr
 		end
 		
 		def guess(round, feedback, guess)
-			#give last guess and last feedback must be rewritten
-			puts "KI #{self.name} guess. This is round #{round}"
+			#computer guess randomly (but keeping the ones that match exactly)
+			puts "\n#####KI #{self.name} guess. This is round #{round}"
+			#before round 2 there is no feedback, so the guess gets complety generated
 			if round > 1
 				kiguess = []
 				i=0
@@ -167,6 +179,7 @@ class Mastermind
 	
 
 	class Board
+	#Board class, just draws the layout 
 		def draw(feedback, guess, round)
 			puts "The status of the Code-Board:"
 			for n in 0..11
